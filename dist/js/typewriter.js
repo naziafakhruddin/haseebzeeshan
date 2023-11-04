8b9909c7e67693fa62f8a7e05f4b07fa310a7942
haseebzeeshan2010 <haseebzeeshan2010@gmail.com>
2023-11-04 20:40:02 +0000

    Modify typewriter speed for writing and deleting

--- dist/js/typewriter.js
+++ dist/js/typewriter.js
@@ -28,10 +28,10 @@ class TypeWriter {
     this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
 
     // Initial Type Speed
-    let typeSpeed = 300;
+    let typeSpeed = 100;
 
     if (this.isDeleting) {
-      typeSpeed /= 2;
+      typeSpeed *= 1.5;
     }
 
     // If word is complete
@@ -45,7 +45,7 @@ class TypeWriter {
       // Move to next word
       this.wordIndex++;
       // Pause before start typing
-      typeSpeed = 500;
+      typeSpeed = 800;
     }
 
     setTimeout(() => this.type(), typeSpeed);
